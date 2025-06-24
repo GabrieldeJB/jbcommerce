2e499dd4c436e687c2d2b1503a42616fa0a099bc
GABRIEL DE JESUS BARROS <98981078+GabrieldeJB@users.noreply.github.com>
2025-06-24 19:16:37 -0300

    Delete src directory

--- src/main/java/com/meddoc/meddoc/model/TipoUsuario.java
+++
@@ -1,6 +0,0 @@
-package com.meddoc.meddoc.model;
-
-public enum TipoUsuario {
-    IDOSO,
-    MEDICO
-} 
--- src/main/resources/static/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Lógica para o menu de navegação, se houver
-    const currentPath = window.location.pathname;
-    document.querySelectorAll('.nav-link').forEach(link => {
-        if (link.getAttribute('href') === currentPath) {
-            link.classList.add('active');
-        }
-    });
-}); 
